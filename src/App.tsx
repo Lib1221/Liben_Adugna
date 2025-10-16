2e93869237dd7dc63ab356648fe8500807953f9d
Liben <adugnaliben65@gmail.com>
2025-10-16 15:53:31 +0300

    finalized v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import SplashCursor from "./components/sections/splashmouse"; // 🟣 Fluid cursor effect
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="min-h-screen w-full p-8 bg-gradient-to-br from-[#070707] to-[#0f0f0f]">
-      <div className="max-w-7xl mx-auto grid grid-cols-12 gap-8">
-        {/* Left sidebar: sticky card */}
+    <div className="relative min-h-screen w-full bg-gradient-to-br from-[#070707] to-[#0f0f0f] overflow-hidden">
+      {/* 🔮 Splash cursor layer */}
+      <SplashCursor />
+
+      {/* 🧱 Main layout */}
+      <div className="relative z-10 p-8 max-w-7xl mx-auto grid grid-cols-12 gap-8">
+        {/* Left sidebar */}
         <div className="col-span-12 lg:col-span-4">
           <Sidebar />
         </div>
